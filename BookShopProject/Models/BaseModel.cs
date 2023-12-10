6d4251da037ae0a600301d6fa411f49d8c9c3245
Miray Karagöz <miray.karagoz@std.nisantasi.edu.tr>
2023-12-10 21:58:06 +0300

    Add project files.

---
+++ BookShopProject/Models/BaseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopProject.Models
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string? Language { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateModified {  get; set; } = DateTime.Now;
+        public DateTime DateDeleted {  get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = false;
+        public bool IsDeleted { get; set; } = true;
+    }
+}
---
+++ BookShopProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BookShopProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
